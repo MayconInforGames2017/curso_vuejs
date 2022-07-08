090dbac88c7a9b5b3804838173554aca6723eb3b
mayconinforgames2014 <maycon_gus2009@hotmail.com>
2022-07-08 12:11:53 -0300

    Criando o servidor com Node e Definindo a lógica do servidor para aplicação Contador

--- vue-socket-server/app.js
+++ vue-socket-server/app.js
@@ -1,3 +1,5 @@
+// Implementação do servidor
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -5,4 +7,25 @@ const io = require('socket.io')(http);
 app.get('/', (req, res) => {
   res.send('socketio...');
 });
+
+io.on('connection', (socket) => {
+  console.log('novo socket conectado');
+
+  socket.on('increment', (counter) => {
+    console.log('increment');
+    io.socket.emit('COUNTER_INCREMENT', counter + 1);
+  });
+
+  socket.on('decrement', (counter) => {
+    console.log('decrement');
+    io.socket.emit('COUNTER_DECREMENT', counter - 1);
+  });
+
+});
+
+http.listen(5000, () => {
+  console.log('Teste na porta 5000');
+});
+
+module.exports = app;
 
