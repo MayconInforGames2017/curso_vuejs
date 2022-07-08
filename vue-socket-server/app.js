524f09253e757c29f193bdb2130becd8cd0be022
mayconinforgames2014 <maycon_gus2009@hotmail.com>
2022-07-08 11:49:56 -0300

    Criando projeto Contador mais Chat integrado

---
+++ vue-socket-server/app.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.send('socketio...');
+});
+
